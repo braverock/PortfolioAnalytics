d8a5d1c09543917036c186057504bd71629ce635
Brian G. Peterson <brian@braverock.com>
2009-11-09 21:54:16 +0000

    - remove constraints fn from this file
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/optimizer@1409 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/optimizer-portfolios.R
+++ R/optimizer-portfolios.R
@@ -32,33 +32,6 @@
 # }
 # ultimately, I'd like to have a real portfolio class, and real constraints classes, but for now we can build Burns-style random portfolios by specifying our box constriants
 
-contraint_v1 <- function(nassets=NULL,min,max,min_mult=NULL,max_mult=NULL)
-{ # based on GPL R-Forge pkg 'roi' by Stefan Thuessel/Kurt Hornik/David Meyer
-  if (is.null(nassets & !is.vector(min) & !is.vector(max)) {
-    stop("You must either specify the number of assets or pass a vector for both min and max")
-  }
-  
-  if (is.vector(min) & is.vector(max)){
-    if (length(min)!=length(max)) { stop("length of min and max must be the same") }
-    nassets = length(min)
-  } else {
-    warning("min and max not passed in as vectors, replicating min and max to length of nassets")
-    min = rep(min,nassets)
-    max = rep(max,nassets)
-  }
-  
-  structure(
-    list(
-      nassets = nassets,
-      min = min,
-      max = max,
-      min_mult = min_mult,
-      max_mult = max_mult,
-      class = c("constraint_v1","constraint")
-    )
-  )
-}
-            
 
 # random portfolios
 
