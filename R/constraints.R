6db3292298f485ea600cda72309fd2dc9381fb51
Brian G. Peterson <brian@braverock.com>
2009-11-10 20:11:35 +0000

    - updates to constraint class
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/optimizer@1410 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/constraints.R
+++ R/constraints.R
@@ -10,32 +10,74 @@
 #
 ###############################################################################
 
-contraint_v1 <- function(nassets=NULL,minc,maxc,min_mult=NULL,max_mult=NULL)
+constraint <- function(assets=NULL,min=NULL,max=NULL,min_mult=NULL,max_mult=NULL)
 { # based on GPL R-Forge pkg roi by Stefan Thuessel,Kurt Hornik,David Meyer
-  if (is.null(nassets) & !is.vector(min) & !is.vector(max)) {
-    stop("You must either specify the number of assets or pass a vector for both min and max")
+  if (is.null(assets) & (is.null(min) | !length(min)>1) & (is.null(max)| !length(max)>1)) {
+    stop("You must either specify the assets or pass a vector for both min and max")
   }
 
-  if (is.vector(min) & is.vector(max)){
+  if(!is.null(assets)){
+    if(is.numeric(assets)){
+      nassets=assets
+      #we passed in a number of assets, so we need to name them and create the vector
+      for(i in 1:assets){
+        assets[i]<-paste("Asset",i,sep=".")
+      }
+    }
+    if(is.character(assets)){
+      names(assets)<-assets  # set names, so that other code can access it,
+      # and doesn't have to know about the character vector
+      nassets=length(assets)
+    }
+  }
+
+  if (length(min)>1 & length(max)>1){
     if (length(min)!=length(max)) { stop("length of min and max must be the same") }
-    nassets = length(min)
   } else {
-    warning("min and max not passed in as vectors, replicating min and max to length of nassets")
-    min = rep(min,nassets)
-    max = rep(max,nassets)
+    warning("min and max not passed in as vectors, replicating min and max to length of length(assets)")
+    min <- rep(min,nassets)
+    max <- rep(max,nassets)
   }
 
+  if(!is.null(min_mult) | !is.null(max_mult)) {
+    if (is.vector(min_mult) & is.vector(max_mult)){
+      if (length(min_mult)!=length(max_mult) ) { stop("length of min_mult and max_mult must be the same") }
+    } else {
+      warning("min_mult and max_mult not passed in as vectors, replicating min_mult and max_mult to length of assets vector")
+      min_mult = rep(min_mult,nassets)
+      max_mult = rep(max_mult,nassets)
+    }
+  }
+  
+  if (!is.null(names(assets))) {
+    assetnames<-names(assets)
+    if(!is.null(min)){
+      names(min)<-assetnames
+      names(max)<-assetnames
+    }
+    if(!is.null(min_mult)){
+      names(min_mult)<-assetnames
+      names(max_mult)<-assetnames
+    }
+  }
+  ## now structure and return
   structure(
     list(
-      nassets = nassets,
+      assets = assets,
       min = min,
       max = max,
       min_mult = min_mult,
       max_mult = max_mult,
-      class = c("constraint_v1","constraint")
-    )
+      objectives = list()
+    ),
+    class=c("v1_constraint","constraint")
   )
 }
+
+is.constraint <- function( x ) {
+  inherits( x, "constraint" )
+}
+
 
 modify_constraint <- function(constraints, ...){
   if (is.null(constraints) | class(constraints)!="constraint"){
@@ -63,3 +105,16 @@ modify_constraint <- function(constraints, ...){
   if(hasArg(max_mult)){constrains$max_mult=max_mult}
   return(constraints)
 }
+
+# assume a structure that includes column names for each asset
+# constraints should be a list by constraint type
+# we have min/max constraints on each asset
+# constraint <- function (x){
+#
+# structure( list(L   = L,
+#                   dir = dir,
+#                   rhs = rhs,
+#                   n_L_constraints = n_L_constraints),
+#             class = c("L_constraint", "constraint"))
+# }
+# ultimately, I'd like to have a real portfolio class, and real constraints classes, but for now we can build Burns-style random portfolios by specifying our box constriants
