5552546fe18c96b2bf5fe59bd821360f25961ff9
Brian G. Peterson <brian@braverock.com>
2009-11-26 14:42:41 +0000

    - minor update to include min_sum and max_sum
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/optimizer@1454 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- demo/constrained_optim.R
+++ demo/constrained_optim.R
@@ -1,7 +1,8 @@
 require("optimizer")
 require("DEoptim")
 data(edhec)
-constraints=constraint(assets = colnames(edhec[, 1:10]), min = 0.01, max = 0.4, weight_seq = generatesequence())
+constraints=constraint(assets = colnames(edhec[, 1:10]), min = 0.01, max = 0.4, min_sum=1, max_sum=1, weight_seq = generatesequence())
+# note that if you wanted to do a random portfolio optimization, mun_sum of .99 and max_sum of 1.01 might be more appropriate
 constraints<-add.objective(constraints, type="return", name="mean", enabled=TRUE)
 constraints<-add.objective(constraints, type="risk_budget", name="ES", enabled=TRUE, p=.95, min_prisk=.05, max_prisk=.15)
 constraints
