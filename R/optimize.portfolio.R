a28d5b50e4ed93e5a88a21d4e86c62bbeaad2b87
Brian G. Peterson <brian@braverock.com>
2009-12-22 19:44:11 +0000

    - fix search for random optimization method
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PortfolioAnalytics@1479 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/optimize.portfolio.R
+++ R/optimize.portfolio.R
@@ -52,13 +52,37 @@ optimize.portfolio <- function(R,constraints,optimize_method=c("DEoptim","random
   
   out=list()
   
+  normalize_weights <- function(w){
+      # normalize results if necessary
+      if(!is.null(constraints$min_sum) | !is.null(constraints$max_sum)){
+          # the user has passed in either min_sum or max_sum constraints for the portfolio, or both.
+          # we'll normalize the weights passed in to whichever boundary condition has been violated
+          # NOTE: this means that the weights produced by a numeric optimization algorithm like DEoptim
+          # might violate your constraints, so you'd need to renormalize them after optimizing
+          # we'll create functions for that so the user is less likely to mess it up.
+          
+          ##' NOTE: need to normalize in the optimization wrapper too before we return, since we've normalized in here
+          ##' In Kris' original function, this was manifested as a full investment constraint
+          if(!is.null(constraints$max_sum) & constraints$max_sum != Inf ) {
+              max_sum=constraints$max_sum
+              if(sum(w)>max_sum) { w<-(max_sum/sum(w))*w } # normalize to max_sum
+          }
+          
+          if(!is.null(constraints$min_sum) & constraints$min_sum != -Inf ) {
+              min_sum=constraints$min_sum
+              if(sum(w)<min_sum) { w<-(min_sum/sum(w))*w } # normalize to min_sum
+          }
+          
+      } # end min_sum and max_sum normalization
+      return(w)
+  }
   if(optimize_method=="DEoptim"){
     stopifnot("package:DEoptim" %in% search() || require("DEoptim",quietly = TRUE))
     # DEoptim does 200 generations by default, so lets set the size of each generation to search_size/200)
     if(hasArg(itermax)) itermax=match.call(expand.dots=TRUE)$itermax else itermax=200
     NP = round(search_size/itermax)
     if(NP>2000) NP=2000
-    if(!hasArg(controlDE)) controlDE = list( VTR = 0 , NP=NP, trace=trace, trystart=5 ) else controlDE=match.call(expand.dots=TRUE)$controlDE
+    if(!hasArg(controlDE)) controlDE = list( NP=NP, itermax=itermax, trace=trace, trystart=5 ) else controlDE=match.call(expand.dots=TRUE)$controlDE
     if(hasArg(VTR)) controlDE$VTR <- match.call(expand.dots=TRUE)$VTR #target number for the objective function
     if(hasArg(F))   controlDE$F  <- match.call(expand.dots=TRUE)$F   # stepsize, default .8
     if(hasArg(CR))  controlDE$CR <- match.call(expand.dots=TRUE)$CR 	 # Crossover probability from interval [0,1]. Default to '0.5'
@@ -81,27 +105,7 @@ optimize.portfolio <- function(R,constraints,optimize_method=c("DEoptim","random
     }
 
     w = as.vector( minw$optim$bestmem)
-    # normalize results if necessary
-    if(!is.null(constraints$min_sum) | !is.null(constraints$max_sum)){
-        # the user has passed in either min_sum or max_sum constraints for the portfolio, or both.
-        # we'll normalize the weights passed in to whichever boundary condition has been violated
-        # NOTE: this means that the weights produced by a numeric optimization algorithm like DEoptim
-        # might violate your constraints, so you'd need to renormalize them after optimizing
-        # we'll create functions for that so the user is less likely to mess it up.
-        
-        ##' NOTE: need to normalize in the optimization wrapper too before we return, since we've normalized in here
-        ##' In Kris' original function, this was manifested as a full investment constraint
-        if(!is.null(constraints$max_sum) & constraints$max_sum != Inf ) {
-            max_sum=constraints$max_sum
-            if(sum(w)>max_sum) { w<-(max_sum/sum(w))*w } # normalize to max_sum
-        }
-        
-        if(!is.null(constraints$min_sum) & constraints$min_sum != -Inf ) {
-            min_sum=constraints$min_sum
-            if(sum(w)<min_sum) { w<-(min_sum/sum(w))*w } # normalize to min_sum
-        }
-        
-    } # end min_sum and max_sum normalization
+    w <- normalize_weights(w)
     names(w) = colnames(R)
 
     out = list(weights=w , objective_measures=constrained_objective(w=w,R=R,constraints,trace=TRUE)$objective_measures,call=call) 
@@ -122,7 +126,9 @@ optimize.portfolio <- function(R,constraints,optimize_method=c("DEoptim","random
       #' if trace=TRUE , store results of foreach in out$random_results
       if(trace) out$random_portfolio_objective_results<-rp_objective_results
       #' loop through results keeping track of the minimum value of rp_objective_results[[objective]]$out
-      min_objective_weights<- NULL #'TODO ## DO SOME SEARCH HERE ##
+      search<-vector(length=length(rp_objective_results))
+      for (i in 1:length(search)) search[i]<-rp_objective_results[[i]]$out
+      min_objective_weights<- normalize_weights(rp_objective_results[[which.min(search)]]$weights)
       #' re-call constrained_objective on the best portfolio, as above in DEoptim, with trace=TRUE to get results for out list
       out$weights<-min_objective_weights
       out$constrained_objective<-constrained_objective(w=min_objective_weights,R=R,constraints,trace=TRUE)$objective_measures
@@ -131,7 +137,7 @@ optimize.portfolio <- function(R,constraints,optimize_method=c("DEoptim","random
   }
     end_t<-Sys.time()
     # print(c("elapsed time:",round(end_t-start_t,2),":diff:",round(diff,2), ":stats: ", round(out$stats,4), ":targets:",out$targets))
-    message(c("elapsed time:",round(end_t-start_t,2)))
+    message(c("elapsed time:",end_t-start_t))
     out$constraints<-constraints
     out$data_summary<-list(first=first(R),last=last(R))
     out$elapsed_time<-end_t-start_t
