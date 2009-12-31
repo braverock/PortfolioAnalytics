07a39b2364b53cf05d98662b3ab99b448835ac9d
Brian G. Peterson <brian@braverock.com>
2009-12-31 13:24:01 +0000

    - demonstrate how to extract weights and calculate returns on rebalancing portfolio optimization
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PortfolioAnalytics@1497 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- demo/constrained_optim.R
+++ demo/constrained_optim.R
@@ -13,6 +13,8 @@ constraints$objectives[[1]]$multiplier=-1
 constraints$objectives[[2]]$target=.05
 # and clean the returns
 constraints$objectives[[2]]$clean="boudt"
+
+print("We'll use a search_size parameter of 1000 for this demo, but realistic portfolios will likely require search_size parameters much larger, the default is 20000 which is almost always large enough for any realistic portfolio and constraints, but will take substantially longer to run.")
 # look for a solution using both DEoptim and random portfolios
 opt_out<-optimize.portfolio(R=edhec[,1:10], constraints, optimize_method="DEoptim", search_size=1000, trace=TRUE)
 #we need a little more wiggle in min/max sum for random portfolios or it takes too long to converge
@@ -22,5 +24,12 @@ opt_out_random<-optimize.portfolio(R=edhec[,1:10], constraints, optimize_method=
 
 # now lets try a portfolio that rebalances quarterly
 opt_out_rebalancing<-optimize.portfolio.rebalancing(R=edhec[,1:10], constraints, optimize_method="DEoptim", search_size=1000, trace=FALSE,rebalance_on='quarters')
+rebalancing_weights<-matrix(nrow=length(opt_out_rebalancing),ncol=length(opt_out_rebalancing[[1]]]$weights))
+rownames(rebalancing_weights)<-names(opt_out_rebalancing)
+colnames(rebalancing_weights)<-names(opt_out_rebalancing[[1]]$weights)
+for(period in 1:length(opt_out_rebalancing)) rebalancing_weights[period,]<-opt_out_rebalancing[[period]]$weights
+rebalancing_returns<-Return.rebalancing(R=edhec,weights=rebalancing_weights)
+charts.PerformanceSummary(rebalancing_returns)
+
 # and now lets rebalance quarterly with 48 mo trailing
 opt_out_trailing<-optimize.portfolio.rebalancing(R=edhec[,1:10], constraints, optimize_method="DEoptim", search_size=1000, trace=FALSE,rebalance_on='quarters',trailing_periods=48,training_period=48)
