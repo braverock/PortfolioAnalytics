aec53ed32f45b1992c806d75c5032b8cd9e5a372
Brian G. Peterson <brian@braverock.com>
2010-02-11 14:35:53 +0000

    - add print method for optimize.portfolio.rebalancing to increase readability of the output
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PortfolioAnalytics@1545 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/generics.R
@@ -0,0 +1,32 @@
+###############################################################################
+# R (http://r-project.org/) Numeric Methods for Optimization of Portfolios
+#
+# Copyright (c) 2004-2010 Kris Boudt, Peter Carl and Brian G. Peterson
+#
+# This library is distributed under the terms of the GNU Public License (GPL)
+# for full details see the file COPYING
+#
+# $Id$
+#
+###############################################################################
+
+print.optimize.portfolio.rebalancing <- function(x) {
+    if(!inherits(x,"optimize.portfolio.rebalancing")) 
+        stop ("passed object is not of class optimize.portfolio.rebalancing")
+    
+    
+    # loop through and show the results and weights
+    cat('Weights:\n')
+    for(i in 1:length(x)){
+        cat(names(x[i]))
+        cat('\n')
+        print(round(x[[i]]$weights,4))
+    }
+    cat('Objective Measures\n')
+    for(i in 1:length(x)){
+        cat(names(x[i]))
+        cat('\n')
+        print(x[[i]]$objective_measures)
+    }
+    
+}
