724f655126a4abe7be5de81c4bf78ff5806b37df
Brian G. Peterson <brian@braverock.com>
2010-02-24 17:27:00 +0000

    - initial commit of trailingFUN wrapper
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PortfolioAnalytics@1589 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/trailingFUN.R
@@ -0,0 +1,70 @@
+###############################################################################
+# R (http://r-project.org/) Numeric Methods for Optimization of Portfolios
+#
+# Copyright (c) 2004-2010 Kris Boudt, Peter Carl and Brian G. Peterson
+#
+# This library is distributed under the terms of the GNU Public License (GPL)
+# for full details see the file COPYING
+#
+# $Id$
+#
+###############################################################################
+
+trailingFUN <- function(R, weights, n=0, FUN, FUNargs, ...) {
+    
+    if (is.null(FUN)) {stop("you must supply a function to apply to")}
+    
+    funname<-FUN
+    FUN<-match.fun(FUN)
+    
+    nargs <-list(...)
+    if(length(nargs)==0) nargs=NULL
+    if (length('...')==0 | is.null('...')) {
+        # rm('...')
+        nargs=NULL
+    }
+    
+    if(!is.null(nrow(R))) R<-R[nrow(R-n):nrow(R),]
+    else R<-R[length(R-n):length(R)]
+    
+    
+    if(is.function(FUN)){
+        .formals  <- formals(FUN)
+        onames <- names(.formals)
+        if(is.list(FUNargs)){
+            #TODO FIXME only do this if R and weights are in the argument list of the fn
+            if(is.null(nargs$R) | !length(nargs$R)==length(R)) nargs$R <- R
+            if(!is.null(nargs$x) | !length(nargs$x)==length(R)) nargs$x <- R
+            
+            if(is.null(nargs$weights)) nargs$weights <- weights
+            
+            pm <- pmatch(names(FUNargs), onames, nomatch = 0L)
+            if (any(pm == 0L))
+                warning(paste("some FUNargs stored for",funname,"do not match"))
+            # this line overwrites the names of things stored in $FUNargs with names from formals.
+            # I'm not sure it's a good idea, so commenting for now, until we prove we need it
+            #names(FUNargs[pm > 0L]) <- onames[pm]
+            .formals[pm] <- FUNargs[pm > 0L]
+            #now add dots
+            if (length(nargs)) {
+                dargs<-nargs
+                pm <- pmatch(names(dargs), onames, nomatch = 0L)
+                names(dargs[pm > 0L]) <- onames[pm]
+                .formals[pm] <- dargs[pm > 0L]
+            }
+            .formals$... <- NULL
+        } else {
+            warning('no FUNargs passed for function')
+        }
+    } else {
+        stop('FUN must specify an R function')
+    }
+    
+    tmp_measure = try((do.call(FUN,.formals)) ,silent=TRUE)
+      
+    if(inherits(tmp_measure,"try-error") | is.na(tmp_measure)) { 
+        message(paste("trailing function generated an error or warning:",tmp_measure))
+    } else {
+        return(tmp_measure)
+    }
+}
