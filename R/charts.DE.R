0fc167b90dc730116658ca996c10a1d0ad2faef0
Peter Carl <pcarl@gsb.uchicago.edu>
2010-04-09 02:30:11 +0000

    - first draft of graphics for DEoptim results
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PortfolioAnalytics@1662 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/charts.DE.R
@@ -0,0 +1,259 @@
+###############################################################################
+# R (http://r-project.org/) Numeric Methods for Optimization of Portfolios
+#
+# Copyright (c) 2004-2010 Kris Boudt, Peter Carl and Brian G. Peterson
+#
+# This library is distributed under the terms of the GNU Public License (GPL)
+# for full details see the file COPYING
+#
+# $Id$
+#
+###############################################################################
+
+#' boxplot of the weight distributions in the random portfolios 
+#' @param DE set of random portfolios created by \code{\link{optimize.portfolio}}
+#' @param neighbors set of 'neighbor' portfolios to overplot
+#' @param las numeric in \{0,1,2,3\}; the style of axis labels
+#'       \describe{
+#'         \item{0:}{always parallel to the axis [\emph{default}],}
+#'         \item{1:}{always horizontal,}
+#'         \item{2:}{always perpendicular to the axis,}
+#'         \item{3:}{always vertical.}
+#'       }
+#' @param xlab a title for the x axis: see \code{\link{title}}
+#' @param cex.lab The magnification to be used for x and y labels relative to the current setting of \code{cex}
+#' @param cex.axis The magnification to be used for axis annotation relative to the current setting of \code{cex}
+#' @param element.color color for the default plot lines
+#' @param ... any other passthru parameters 
+#' @param main an overall title for the plot: see \code{\link{title}}
+#' @seealso \code{\link{optimize.portfolio}}
+#' @export
+chart.Weights.DE <- function(DE, neighbors = NULL, ..., main="Weights", las = 3, xlab=NULL, cex.lab = 1, element.color = "darkgray", cex.axis=0.8){
+# Specific to the output of the random portfolio code with constraints
+    # @TODO: check that DE is of the correct class
+    columnnames = names(DE$weights)
+    numassets = length(columnnames)
+
+    if(is.null(xlab))
+        minmargin = 3
+    else
+        minmargin = 5
+    if(main=="") topmargin=1 else topmargin=4
+    if(las > 1) {# set the bottom border to accommodate labels
+        bottommargin = max(c(minmargin, (strwidth(columnnames,units="in"))/par("cin")[1])) * cex.lab
+        if(bottommargin > 10 ) {
+            bottommargin<-10
+            columnnames<-substr(columnnames,1,19)
+            # par(srt=45) #TODO figure out how to use text() and srt to rotate long labels
+        }
+    }
+    else {
+        bottommargin = minmargin
+    }
+    par(mar = c(bottommargin, 4, topmargin, 2) +.1)
+    plot(DE$weights, type="b", col="blue", axes=FALSE, xlab='', ylim=c(0,max(DE$constraints$max)), ylab="Weights", main=main, pch=16, ...)
+    points(DE$constraints$min, type="b", col="darkgray", lty="solid", lwd=2, pch=24)
+    points(DE$constraints$max, type="b", col="darkgray", lty="solid", lwd=2, pch=25)
+#     if(!is.null(neighbors)){ 
+#         if(is.vector(neighbors)){
+#             xtract=extractStats(DE)
+#             weightcols<-grep('w\\.',colnames(xtract)) #need \\. to get the dot 
+#             if(length(neighbors)==1){
+#                 # overplot nearby portfolios defined by 'out'
+#                 orderx = order(xtract[,"out"])
+#                 subsetx = head(xtract[orderx,], n=neighbors)
+#                 for(i in 1:neighbors) points(subsetx[i,weightcols], type="b", col="lightblue")
+#             } else{
+#                 # assume we have a vector of portfolio numbers
+#                 subsetx = xtract[neighbors,weightcols]
+#                 for(i in 1:length(neighbors)) points(subsetx[i,], type="b", col="lightblue")
+#             }      
+#         }
+#         if(is.matrix(neighbors) | is.data.frame(neighbors)){
+#             # the user has likely passed in a matrix containing calculated values for risk.col and return.col
+#             nbweights<-grep('w\\.',colnames(neighbors)) #need \\. to get the dot
+#             for(i in 1:nrow(neighbors)) points(as.numeric(neighbors[i,nbweights]), type="b", col="lightblue")
+#             # note that here we need to get weight cols separately from the matrix, not from xtract
+#             # also note the need for as.numeric.  points() doesn't like matrix inputs
+#         }
+#     }
+    
+#     points(DE$weights, type="b", col="blue", pch=16)
+    axis(2, cex.axis = cex.axis, col = element.color)
+    axis(1, labels=columnnames, at=1:numassets, las=las, cex.axis = cex.axis, col = element.color)
+    box(col = element.color)
+
+}
+
+#' classic risk return scatter of DEoptim results
+#' 
+#' @param DE set of portfolios created by \code{\link{optimize.portfolio}}
+#' @param neighbors set of 'neighbor' portfolios to overplot, see Details
+#' @param return.col string matching the objective of a 'return' objective, on vertical axis
+#' @param risk.col string matching the objective of a 'risk' objective, on horizontal axis
+#' @param ... any other passthru parameters 
+#' @param cex.axis The magnification to be used for axis annotation relative to the current setting of \code{cex}
+#' @param element.color color for the default plot scatter points
+#' @seealso \code{\link{optimize.portfolio}}
+#' @export
+chart.Scatter.DE <- function(DE, neighbors = NULL, return.col='mean', risk.col='ES', ..., element.color = "darkgray", cex.axis=0.8){
+    # more or less specific to the output of the random portfolio code with constraints
+    # will work to a point with other functions, such as optimize.porfolio.parallel
+    # there's still a lot to do to improve this.
+    xtract = extractStats(DE)
+    columnnames = colnames(xtract)
+    #return.column = grep(paste("objective_measures",return.col,sep='.'),columnnames)
+    return.column = pmatch(return.col,columnnames)
+    if(is.na(return.column)) {
+        return.col = paste(return.col,return.col,sep='.')
+        return.column = pmatch(return.col,columnnames)
+    }
+    #risk.column = grep(paste("objective_measures",risk.col,sep='.'),columnnames)
+    risk.column = pmatch(risk.col,columnnames)
+    if(is.na(risk.column)) {
+        risk.col = paste(risk.col,risk.col,sep='.')
+        risk.column = pmatch(risk.col,columnnames)
+    }
+    
+    if(is.na(return.column) | is.na(risk.column)) stop(return.col,' or ',risk.col, ' do not match extractStats output')
+    
+    plot(xtract[,risk.column],xtract[,return.column], xlab=risk.col, ylab=return.col, col="darkgray", axes=FALSE, ...)
+
+    if(!is.null(neighbors)){ 
+        if(is.vector(neighbors)){
+            if(length(neighbors)==1){
+                # overplot nearby portfolios defined by 'out'
+                orderx = order(xtract[,"out"]) #TODO this won't work if the objective is anything othchart.Scatter.er than mean
+                subsetx = head(xtract[orderx,], n=neighbors)
+            } else{
+                # assume we have a vector of portfolio numbers
+                subsetx = xtract[neighbors,]
+            }
+            points(subsetx[,risk.column], subsetx[,return.column], col="lightblue", pch=1)
+        }
+        if(is.matrix(neighbors) | is.data.frame(neighbors)){
+            # the user has likely passed in a matrix containing calculated values for risk.col and return.col      
+            rtc = pmatch(return.col,columnnames)
+            if(is.na(rtc)) {
+                rtc = pmatch(paste(return.col,return.col,sep='.'),columnnames)
+            }
+            rsc = pmatch(risk.col,columnnames)
+            if(is.na(rsc)) {
+                risk.column = pmatch(paste(risk.col,risk.col,sep='.'),columnnames)
+            }
+            for(i in 1:nrow(neighbors)) points(neighbors[i,rsc], neighbors[i,rtc], col="lightblue", pch=1)
+        }
+    }
+    
+#     points(xtract[1,risk.column],xtract[1,return.column], col="orange", pch=16) # overplot the equal weighted (or seed)
+    #check to see if portfolio 1 is EW  DE$random_portoflios[1,] all weights should be the same
+#     if(!isTRUE(all.equal(DE$random_portfolios[1,][1],1/length(DE$random_portfolios[1,]),check.attributes=FALSE))){
+        #show both the seed and EW if they are different 
+        #NOTE the all.equal comparison could fail above if the first element of the first portfolio is the same as the EW weight, 
+        #but the rest is not, shouldn't happen often with real portfolios, only toy examples
+#         points(xtract[2,risk.column],xtract[2,return.column], col="green", pch=16) # overplot the equal weighted (or seed)
+#     }
+
+    ## Draw solution trajectory
+    w.traj = unique(DE$DEoutput$member$bestmemit)
+    rows = nrow(w.traj)
+    rr = matrix(nrow=rows, ncol=2)
+    for(i in 1:rows){
+        w = w.traj[i,]
+        x = constrained_objective(w=w, R=indexes[,1:4], constraints=EqRiskConstr, trace=TRUE)
+        rr[i,1] = x$objective_measures$CVaR$MES
+        rr[i,2] = x$objective_measures$pamean
+    }
+
+    colors2 = colorRamp(c("blue","lightblue"))
+    colortrail = rgb(colors2((0:rows)/rows),max=255)
+    for(i in 1:rows){
+    points(rr[i,1], rr[i,2], pch=1, col = colortrail[rows-i+1])
+    }
+
+    for(i in 2:rows){
+        segments(rr[i,1], rr[i,2], rr[i-1,1], rr[i-1,2],col = colortrail[rows-i+1], lty = 1, lwd = 2)
+    }
+
+    ## @TODO: Generalize this to find column containing the "risk" metric
+    if(length(names(DE)[which(names(DE)=='constrained_objective')])) {
+        result.slot<-'constrained_objective'
+    } else {
+        result.slot<-'objective_measures'
+    }
+    objcols<-unlist(DE[[result.slot]])
+    names(objcols)<-PortfolioAnalytics:::name.replace(names(objcols))
+    return.column = pmatch(return.column,names(objcols))
+    if(is.na(return.column)) {
+        return.col = paste(return.col,return.col,sep='.')
+        return.column = pmatch(return.col,names(objcols))
+    }
+    risk.column = pmatch(risk.col,names(objcols))
+    if(is.na(risk.column)) {
+        risk.col = paste(risk.col,risk.col,sep='.')
+        risk.column = pmatch(risk.col,names(objcols))
+    }
+    if(is.na(return.column) | is.na(risk.column)) warning(return.col,' or ',risk.col, ' do  not match extractStats output of $objective_measures slot')
+    points(objcols[risk.column], objcols[return.column], col="blue", pch=16) # optimal
+    axis(1, cex.axis = cex.axis, col = element.color)
+    axis(2, cex.axis = cex.axis, col = element.color)
+    box(col = element.color)
+}
+
+#' scatter and weights chart  for random portfolios
+#' 
+#' \code{neighbors} may be specified in three ways.  
+#' The first is as a single number of neighbors.  This will extract the \code{neighbors} closest 
+#' portfolios in terms of the \code{out} numerical statistic.
+#' The second method consists of a numeric vector for \code{neighbors}.
+#' This will extract the \code{neighbors} with portfolio index numbers that correspond to the vector contents.
+#' The third method for specifying \code{neighbors} is to pass in a matrix.  
+#' This matrix should look like the output of \code{\link{extractStats}}, and should contain
+#' \code{risk.col},\code{return.col}, and weights columns all properly named.  
+#' 
+#' @param DE set of random portfolios created by \code{\link{optimize.portfolio}}
+#' @param ... any other passthru parameters 
+#' @param risk.col string name of column to use for risk (horizontal axis)
+#' @param return.col string name of column to use for returns (vertical axis)
+#' @param neighbors set of 'neighbor portfolios to overplot
+#' @param main an overall title for the plot: see \code{\link{title}}
+#' @seealso 
+#' \code{\link{optimize.portfolio}}
+#' \code{\link{extractStats}}
+#' @export
+charts.DE <- function(DE, risk.col, return.col, neighbors=NULL, main="DEoptim Portfolios", ...){
+# Specific to the output of the random portfolio code with constraints
+    # @TODO: check that DE is of the correct class
+    op <- par(no.readonly=TRUE)
+    layout(matrix(c(1,2)),height=c(2,1.5),width=1)
+    par(mar=c(4,4,4,2))
+    chart.Scatter.DE(DE, risk.col=risk.col, return.col=return.col, neighbors=neighbors, main=main, ...)
+    par(mar=c(2,4,0,2))
+    chart.Weights.DE(DE, main="", neighbors=neighbors, ...)
+    par(op)
+}
+
+#TODO make chart.DE into a plot() method or methods
+
+#' plot method for optimize.portfolio output
+#' 
+#' scatter and weights chart  for random portfolios
+#' 
+#' \code{neighbors} may be specified in three ways.  
+#' The first is as a single number of neighbors.  This will extract the \code{neighbors} closest 
+#' portfolios in terms of the \code{out} numerical statistic.
+#' The second method consists of a numeric vector for \code{neighbors}.
+#' This will extract the \code{neighbors} with portfolio index numbers that correspond to the vector contents.
+#' The third method for specifying \code{neighbors} is to pass in a matrix.  
+#' This matrix should look like the output of \code{\link{extractStats}}, and should contain
+#' \code{risk.col},\code{return.col}, and weights columns all properly named.  
+#' @param x set of portfolios created by \code{\link{optimize.portfolio}}
+#' @param ... any other passthru parameters 
+#' @param risk.col string name of column to use for risk (horizontal axis)
+#' @param return.col string name of column to use for returns (vertical axis)
+#' @param neighbors set of 'neighbor portfolios to overplot
+#' @param main an overall title for the plot: see \code{\link{title}}
+#' @export
+plot.optimize.portfolio <- function(x, ...,  return.col='mean', risk.col='ES',  neighbors=NULL, main='optimized portfolio plot') {
+    charts.DE(DE=x, risk.col=risk.col, return.col=return.col, neighbors=neighbors, main=main, ...)
+}
