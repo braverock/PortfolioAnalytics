dac6df5a9ee6a034a04736b1259281dc808b3d7c
Kris Boudt <kris.boudt@econ.kuleuven.be>
2011-08-18 07:40:08 +0000

    corrected numbers referring to previous version, made small changes in the text
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PortfolioAnalytics@1842 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/paper_analysis/illustration/Effect_Feasible_Space.R
+++ sandbox/paper_analysis/illustration/Effect_Feasible_Space.R
@@ -24,7 +24,10 @@ sigma1 = sigma2 = matrix ( rep(rho1,N^2) , ncol = N )
 sigma3 = sigma4 = matrix ( rep(rho2,N^2) , ncol = N )
 sigma5 = sigma6 = matrix ( rep(rho3,N^2) , ncol = N )
 diag(sigma1) = diag(sigma2) = diag(sigma3) = diag(sigma4) = diag(sigma5) = diag(sigma6) =1;
-sigma2[2,2] = sigma4[2,2] = sigma6[2,2] = 4;
+sigma2[2,2] = sigma4[2,2] = sigma6[2,2] = 4; 
+sigma2[1,2]=sigma2[2,1]=2*sigma2[1,2];
+sigma4[1,2]=sigma4[2,1]=2*sigma4[1,2];
+sigma6[1,2]=sigma6[2,1]=2*sigma6[1,2];
 
 w = as.matrix( rep(0.5,2) ) 
 
@@ -176,7 +179,7 @@ text( x = 0.1 , y = 0.95*max(vpccvar1) , labels=expression( rho*"="*0.5) )
 if(showweightlines){ abline( h = 0.4 , lty = 3) ; abline( h = 0.6 , lty = 3 )}
 # Indicate ERC portfolio
 objective = function( w1 ){
-   w = as.matrix( c(w1,1-w1) ); return( h - percCompCVaR( w = w , mu = mu1 , sigma = sigma1 , alpha = alpha )[1] ) }
+   w = as.matrix( c(w1,1-w1) ); return( h - percCompCVaR( w = w , mu = mu1 , sigma = sigma5 , alpha = alpha )[1] ) }
 h=0.5; ERC = uniroot( objective , lower = 0 , upper = 1)$root
 print(ERC); print( 1/2 ) ; 
 text( x = ERC , y = 0.5 , labels="ERC" , srt = srt )
@@ -235,7 +238,7 @@ dev.off()
 
 postscript('sensitivity_rho50.eps') 
 
-par( mfrow = c(3,1) , las = 1 , mar=c(2.2,6.5,2,1.5)  , cex=1.2 , cex.main=1.2)
+par( mfrow = c(2,2) , las = 1 , mar=c(4.2,4.5,3,1.5)  , cex=1.2 , cex.main=1.2)
 
 #---------------------------------------------------------------------
 
@@ -243,8 +246,8 @@ showweightlines = TRUE;
 
 
 # !!!! Setup 7: mu1, sigma5
-plot( vw1 , vpccvar7 , main = expression( mu[1]*"="*mu[2]*"=0 and "*sigma[1]*"="*sigma[2]*"=1") , type="l",
-   ylab="Perc CVaR asset 1 \n in function of its weight" )
+plot( vw1 , vpccvar7 , main = expression( mu[1]*"="*mu[2]*"=0, "*sigma[1]*"="*sigma[2]*"=1 and "*rho*"=0.5") , type="l",
+   ylab="Perc CVaR asset 1", xlab="Weight asset 1" )
 #text( x = 0.1 , y = 0.95*max(vpccvar7) , labels=expression( rho*"="*0.5) )
 # %C[2]CVaR <= 60% implies %C[1]CVaR >= 40%
 if(showweightlines){ abline( h = 0.4 , lty = 3) ; abline( h = 0.6 , lty = 3 )}
@@ -261,8 +264,8 @@ if(showweightlines){ abline( v = v1 , lty = 3) ; abline( v = v2 , lty = 3 ) }
 
 
 # !!!! Setup 9: mu1, sigma6
-plot( vw1 , vpccvar9 , main = expression( mu[1]*"="*mu[2]*"=0 and "* sigma[1]*"=1, "*sigma[2]*"=2   ") , type="l" ,
-    ylab="Perc CVaR asset 1 \n in function of its weight" )
+plot( vw1 , vpccvar9 , main = expression( mu[1]*"="*mu[2]*"=0, "* sigma[1]*"=1, "*sigma[2]*"=2 and "*rho*"=0.5") , type="l" ,
+    ylab="Perc CVaR asset 1", xlab="Weight asset 1" )
 #text( x = 0.1 , y = 0.95*max(vpccvar9)  , labels=expression( rho*"="*0.5) )
 if(showweightlines){   abline( h = 0.4 , lty = 3) ; abline( h = 0.6 , lty = 3 )   }
 # uniroot: find value for which it is 0.4 and 0.6
@@ -277,10 +280,11 @@ h=0.4 ; v1 = uniroot( objective , lower = 0 , upper = 1)$root
 h=0.6; v2 = uniroot( objective , lower = 0 , upper = 1)$root
 if(showweightlines){ abline( v = v1 , lty = 3) ; abline( v = v2 , lty = 3 ) }
 
+#par( mfrow = c(2,2) , las = 1 , mar=c(4,4.5,2,1.5)  , cex=1.2 , cex.main=1.2)
 
 # !!!! Setup 8: mu2, sigma5
-plot( vw1 , vpccvar8 , main = expression( mu[1]*"=1, "*mu[2]*"=0 and "*sigma[1]*"="*sigma[2]*"=1"), type="l" , 
- ylab="Perc CVaR asset 1 \n in function of its weight") 
+plot( vw1 , vpccvar8 , main = expression( mu[1]*"=1, "*mu[2]*"=0,  "*sigma[1]*"="*sigma[2]*"=1 and "*rho*"=0.5"), type="l" , 
+ ylab="Perc CVaR asset 1 ", xlab = "Weight asset 1") 
 #text( x = 0.1 , y = 0.95*max(vpccvar8)  , labels=expression( rho*"="*0.5) )
 if(showweightlines){   abline( h = 0.4 , lty = 3) ; abline( h = 0.6 , lty = 3 )   }
 # uniroot: find value for which it is 0.4 and 0.6
@@ -294,6 +298,26 @@ h=0.5; ERC = uniroot( objective , lower = 0 , upper = 1)$root
 h=0.4 ; v1 = uniroot( objective , lower = 0 , upper = 1)$root
 h=0.6; v2 = uniroot( objective , lower = 0 , upper = 1)$root
 if(showweightlines){ abline( v = v1 , lty = 3) ; abline( v = v2 , lty = 3 ) }
+
+
+
+# !!!! Setup 8: mu1, sigma3
+plot( vw1 , vpccvar4 , main = expression(mu[1]*"="*mu[2]*"=0, "*sigma[1]*"="*sigma[2]*"=1 and "*rho*"=0"), type="l" , 
+ ylab="Perc CVaR asset 1 ", xlab = "Weight asset 1") 
+#text( x = 0.1 , y = 0.95*max(vpccvar8)  , labels=expression( rho*"="*0.5) )
+if(showweightlines){   abline( h = 0.4 , lty = 3) ; abline( h = 0.6 , lty = 3 )   }
+# uniroot: find value for which it is 0.4 and 0.6
+objective = function( w1 ){
+   w = as.matrix( c(w1,1-w1) );
+   return( h - percCompCVaR( w = w , mu = mu1 , sigma = sigma3 , alpha = alpha )[1] )
+}
+h=0.5; ERC = uniroot( objective , lower = 0 , upper = 1)$root
+#text( x = ERC , y = 0.5 , labels="ERC" )
+# uniroot: find value for which it is 0.4 and 0.6
+h=0.4 ; v1 = uniroot( objective , lower = 0 , upper = 1)$root
+h=0.6; v2 = uniroot( objective , lower = 0 , upper = 1)$root
+if(showweightlines){ abline( v = v1 , lty = 3) ; abline( v = v2 , lty = 3 ) }
+
 
 dev.off()
 
