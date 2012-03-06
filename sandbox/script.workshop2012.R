3de37885b44920bf94c89e8e108028eaca8ce8e3
Peter Carl <pcarl@gsb.uchicago.edu>
2012-03-06 01:45:04 +0000

    Parses EDHEC data from 'history.csv' file into xts object
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PortfolioAnalytics@1858 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/script.workshop2012.R
+++ sandbox/script.workshop2012.R
@@ -5,6 +5,7 @@
 ### Load the necessary packages
 # Include optimizer and multi-core packages
 library(PortfolioAnalytics)
+require(PerformanceAnalytics)
 require(xts)
 require(DEoptim)
 require(doMC)
@@ -23,6 +24,15 @@ require(TTR)
 #### US Eq LS
 #### Macro
 #### CTA
+#### Distressed
+
+# Download the following file to the working directory:
+# http://www.edhec-risk.com/indexes/pure_style/data/table/history.csv
+### @TODO: Is there a way to download it directly? Maybe not, seems to require a login
+x=read.csv(file="history.csv", sep=";", header=TRUE, check.names=FALSE)
+x.dates = as.Date(x[,1], format="%d/%m/%Y")
+x.data = apply(x[,-1], MARGIN=2, FUN=function(x){as.numeric(sub("%","", x, fixed=TRUE))/100}) # get rid of percentage signs
+edhec = xts(x.data, order.by=x.dates)
 
 # Statistical analysis of hedge fund indexes
 ## Returns through time
