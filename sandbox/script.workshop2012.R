6e2e6ffaabadcb7cf381fd865221a4edeb01dcaa
Peter Carl <pcarl@gsb.uchicago.edu>
2012-03-09 22:30:28 +0000

    - added buoy portfolios with simple metrics to be modified - moved data parsing out into separate scripts
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PortfolioAnalytics@1861 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/script.workshop2012.R
+++ sandbox/script.workshop2012.R
@@ -5,16 +5,22 @@
 ### Load the necessary packages
 # Include optimizer and multi-core packages
 library(PortfolioAnalytics)
-require(PerformanceAnalytics)
 require(quantmod)
 require(DEoptim)
 require(doMC)
 registerDoMC()
 require(TTR)
-require(RQuantLib) # Needed for calc'ing bond returns
+# Available on r-forge
+require(FactorAnalytics) # development version > build 
 
 ####### Script WBS
 # Parse data from EDHEC or HFRI
+## Just load the data from packages
+### See script.buildEDHEC.R and script.buildFactors.R
+data(edhec)
+data(factors)
+
+
 ## Which styles?
 ### Relative value
 #### FIA
@@ -27,14 +33,6 @@ require(RQuantLib) # Needed for calc'ing bond returns
 #### CTA
 #### Distressed
 
-# Download the following file to the working directory:
-# http://www.edhec-risk.com/indexes/pure_style/data/table/history.csv
-### @TODO: Is there a way to download it directly? Maybe not, seems to require a login
-x=read.csv(file="history.csv", sep=";", header=TRUE, check.names=FALSE)
-x.dates = as.Date(x[,1], format="%d/%m/%Y")
-x.data = apply(x[,-1], MARGIN=2, FUN=function(x){as.numeric(sub("%","", x, fixed=TRUE))/100}) # get rid of percentage signs
-edhec = xts(x.data, order.by=x.dates)
-colnames(edhec)
 # Drop some indexes and reorder
 edhec.R = edhec[,c("Convertible Arbitrage", "Equity Market Neutral","Fixed Income Arbitrage", "Event Driven", "CTA Global", "Global Macro", "Long/Short Equity")]
 
@@ -49,70 +47,20 @@ chart.CumReturns(edhec.R, main = "EDHEC Index Returns", xaxis = FALSE, legend.lo
 par(mar = c(5, 4, 0, 2))
 chart.Drawdown(edhec.R, main = "", ylab = "Drawdown", colorset = rainbow8equal, cex.axis=.6, cex.lab=.7)
 par(op)
+
 ## Distributions
+
 ## Risk
-postscript(file="EDHEC-BarVaR-Returns.eps", height=6, width=5, paper="special", horizontal=FALSE, onefile=FALSE)
+postscript(file="EDHEC-BarVaR.eps", height=6, width=5, paper="special", horizontal=FALSE, onefile=FALSE)
 # Generate charts of EDHEC index returns with ETL and VaR through time
-charts.BarVaR(edhec.R, p=(1-1/12), gap=36, main="EDHEC Index Returns", clean='boudt', show.cleaned=TRUE, show.greenredbars=TRUE, methods=c("ModifiedES", "ModifiedVaR"), show.endvalue=TRUE, colorset=rep("black", NCOL(edhec.R)))
-# @TODO change the colorset of the names to match the other charts
+charts.BarVaR(edhec.R, p=(1-1/12), gap=36, main="EDHEC Index Returns", clean='boudt', show.cleaned=TRUE, show.greenredbars=TRUE, methods=c("ModifiedES", "ModifiedVaR"), show.endvalue=TRUE, colorset=rainbow8equal)
 dev.off()
-## Returns and Risk
+
+## Returns and Risk Scatter
+
 ## Autocorrelation
 
-# Acquire data for factors
 
-## Factor set of five commonly used factors
-
-### Equities
-# Get S&P price returns from FRED for now, TR later
-# @TODO: Get total returns for S&P factor rather than price returns
-getSymbols("SP500", src="FRED") # daily price series
-# Calculate monthly returns
-SP500.R=monthlyReturn(SP500)
-colnames(SP500.R)="SP500"
-
-### Bonds
-# Calculate total returns from the yeild of the 10 year constant maturity index maintained by the Fed
-getSymbols("GS10", src="FRED") #load US Treasury 10y yields from FRED
-# Dates should be end of month, not beginning of the month as reported
-index(GS10) = as.Date(as.yearmon(index(GS10)), frac=1)
-GS10.pr <- GS10  #set this up to hold price returns
-GS10.pr[1,1] <- 0
-colnames(GS10.pr) <- "Price Return"
-for (i in 1:(NROW(GS10)-1)) {
-  GS10.pr[i+1,1] <- FixedRateBondPriceByYield(yield=GS10[i+1,1]/100, issueDate=Sys.Date(), maturityDate=advance("UnitedStates/GovernmentBond", Sys.Date(), 10, 3), rates=GS10[i,1]/100,period=2)[1]/100-1
-}
-#total return will be the price return + yield/12 for one month
-GS10.R <- GS10.pr + lag(GS10,k=1)/12/100
-colnames(GS10.R)<-"GS10"
-
-### Currencies
-# Trade Weighted U.S. Dollar Index: Major Currencies - TWEXMMTH
-getSymbols("TWEXMMTH", src="FRED") # index values
-# Dates should be end of month, not beginning of the month as reported
-index(TWEXMMTH) = as.Date(as.yearmon(index(TWEXMMTH)), frac=1)
-USDI.R=ROC(TWEXMMTH)
-
-### Credit Spread
-# Yield spread of Merrill Lynch High-Yield Corporate Master II Index minus 10-year Treasury
-getSymbols("BAMLH0A0HYM2EY",src="FRED")
-CREDIT=BAMLH0A0HYM2EY-GS10
-
-### Liquidity?
-# getSymbols("TB3MS",src="FRED")
-# index(TB3MS) = as.Date(as.yearmon(index(TB3MS)), frac=1)
-# getSymbols("MED3",src="FRED")
-# index(MED3) = as.Date(as.yearmon(index(MED3)), frac=1)
-# TED=MED3-TB3MS
-
-### Real estate
-### Commodities
-## What benchmarks?
-### S&P 500 TR or MSCI World
-### US 10y or MSCI index?
-### USD index
-### NAREIT
-### DJUBS Commodities index
 
 # Set up objectives as buoys
 ## Equal contribution to
@@ -135,9 +83,21 @@ CREDIT=BAMLH0A0HYM2EY-GS10
 
 # Forecast returns
 ## Start with pamean but don't use it in the presentation
+### Create a small weighted annualized trailing-period mean wrapper function
+pamean <- function(n=12, R, weights, geometric=TRUE)
+{ sum(Return.annualized(last(R,n), geometric=geometric)*weights) }
+
+paEMA <- function(n=10, R, weights, ...)
+{# call Exponential Moving Average from TTR, return the last observation
+  sum((12*last(apply(R,2,FUN=TTR::EMA,n=n)))*weights)
+}
+
+pasd <- function(R, weights){
+  as.numeric(StdDev(R=R, weights=weights)*sqrt(12)) # hardcoded for monthly data
+}
 ## Apply multi-factor model
 ## Show fit
-## ADD MORE HERE
+## ADD MORE DETAIL HERE
 
 # Forecast risk
 ## Historical realized
@@ -146,7 +106,120 @@ CREDIT=BAMLH0A0HYM2EY-GS10
 # Run each of the objective portfolios as of a Date - Dec2010?
 ## Combined scatter with overlaid objectives, starting portfolio
 ### Mean-variance plot
-### Mean-mETL plot?
+
+# A set of box constraints used to initialize all the bouy portfolios
+init.constr <- constraint(assets = colnames(edhec.R),
+  min = .05, # minimum position weight
+  max = .5, #1, # maximum position weight
+  min_sum=0.99, # minimum sum of weights must be equal to 1-ish
+  max_sum=1.01, # maximum sum must also be about 1
+  weight_seq = generatesequence() 
+  )
+
+init.constr <- add.objective(constraints=init.constr,
+  type="return", # the kind of objective this is
+  name="pamean",
+  enabled=TRUE, # enable or disable the objective
+  multiplier=0, # calculate it but don't use it in the objective
+  arguments = list(n=36)
+  )
+
+init.constr <- add.objective(init.constr,
+  type="risk", # the kind of objective this is
+  name="pasd", # the function to minimize
+  enabled=TRUE, # enable or disable the objective
+  multiplier=0, # calculate it but don't use it in the objective
+  arguments=list()
+  )
+
+rp = random_portfolios(rpconstraints=init.constr, permutations=1000)
+
+### BUOY 1: Constrained Mean-StdDev Portfolio
+MeanSD.constr <- init.constr
+# Turn back on the return and sd objectives
+MeanSD.constr$objectives[[1]]$multiplier = -1 # pamean
+MeanSD.constr$objectives[[2]]$multiplier = 1 # pasd
+# Evaluate the constraint object with Random Portfolios
+MeanSD.RND<-optimize.portfolio(R=edhec.R,
+  constraints=MeanSD.constr,
+  optimize_method='random',
+  search_size=1000, trace=TRUE, verbose=TRUE,
+  rp=rp) # use the same random portfolios generated above
+
+### BUOY 2: Constrained Mean-mETL Portfolio
+MeanmETL.constr <- init.constr
+# Turn on the return objective
+MeanmETL.constr$objectives[[1]]$multiplier = -1 # pamean
+# Add a mETL risk objective to the constraints
+MeanmETL.constr <- add.objective(MeanmETL.constr,
+  type="risk", # the kind of objective this is
+  name="CVaR", # the function to minimize
+  enabled=TRUE, # enable or disable the objective
+  arguments=list(p=(1-1/12), clean="boudt")
+  )
+# Evaluate the constraint object with Random Portfolios
+MeanmETL.RND<-optimize.portfolio(R=edhec.R,
+  constraints=MeanmETL.constr,
+  optimize_method='random',
+  search_size=1000, trace=TRUE, verbose=TRUE,
+  rp=rp) # use the same random portfolios generated above
+
+### BUOY 3: Constrained Minimum Variance Portfolio
+MinSD.constr <- init.constr
+# Turn back on the sd objectives
+MinSD.constr$objectives[[2]]$multiplier = 1 # StdDev
+# Evaluate the constraint object with Random Portfolios
+MinSD.RND<-optimize.portfolio(R=edhec.R,
+  constraints=MinSD.constr,
+  optimize_method='random',
+  search_size=1000, trace=TRUE, verbose=TRUE,
+  rp=rp) # use the same random portfolios generated above
+
+### BUOY 4: Constrained Minimum mETL Portfolio
+MinmETL.constr <- add.objective(init.constr,
+  type="risk", # the kind of objective this is
+  name="CVaR", # the function to minimize
+  enabled=TRUE, # enable or disable the objective
+  arguments=list(p=(1-1/12), clean="boudt")
+  )
+# Evaluate the constraint object with Random Portfolios
+MinmETL.RND<-optimize.portfolio(R=edhec.R,
+  constraints=MinmETL.constr,
+  optimize_method='random',
+  search_size=1000, trace=TRUE, verbose=TRUE,
+  rp=rp) # use the same random portfolios generated above
+
+### BUOY 5: Constrained Equal Variance Contribution Portfolio
+EqSD.constr <- add.objective(init.constr, type="risk_budget", name="StdDev",  enabled=TRUE, min_concentration=TRUE, arguments = list(clean='boudt', p=(1-1/12)))
+# Evaluate the constraint object with Random Portfolios
+EqSD.RND<-optimize.portfolio(R=edhec.R,
+  constraints=EqSD.constr,
+  optimize_method='random',
+  search_size=1000, trace=TRUE, verbose=TRUE,
+  rp=rp) # use the same random portfolios generated above
+
+### BUOY 6: Constrained Equal mETL Contribution Portfolio
+EqmETL.constr <- add.objective(init.constr, type="risk_budget", name="CVaR",  enabled=TRUE, min_concentration=TRUE, arguments = list(clean='boudt', p=(1-1/12)))
+# Evaluate the constraint object with Random Portfolios
+EqmETL.RND<-optimize.portfolio(R=edhec.R,
+  constraints=EqmETL.constr,
+  optimize_method='random',
+  search_size=1000, trace=TRUE, verbose=TRUE,
+  rp=rp) # use the same random portfolios generated above
+
+### BUOY 7: Equal Weight Portfolio
+# There's only one:
+# Rebalance an equal-weight portfolio annually
+dates=c(as.Date("1999-12-31"),time(edhec.R[endpoints(edhec.R, on="years")]))
+weights = xts(matrix(rep(1/NCOL(edhec.R),length(dates)*NCOL(edhec.R)), ncol=NCOL(edhec.R)), order.by=dates)
+colnames(weights)= colnames(edhec.R)
+EqWgt = Return.rebalancing(edhec.R,weights)
+
+# Chart EqWgt Results
+postscript(file="EqWgtPlot1.eps", height=6, width=5, paper="special", horizontal=FALSE, onefile=FALSE)
+charts.PerformanceSummary(EqWgt, main="Eq Wgt Portfolio", methods=c("ModifiedVaR", "ModifiedES"), p=(1-1/12), clean='boudt', show.cleaned=TRUE, gap=36, colorset=black, lwd=3)
+dev.off()
+
 ### Comparison of portfolio weights plot
 
 # Historical performance of each buoy portfolio
