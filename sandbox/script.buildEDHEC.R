f21146093242ff897a2e5f2bd60b0ac062207c23
Peter Carl <pcarl@gsb.uchicago.edu>
2012-04-17 11:09:31 +0000

    - added calculation of quarterly returns
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PortfolioAnalytics@1901 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/script.buildEDHEC.R
+++ sandbox/script.buildEDHEC.R
@@ -15,3 +15,18 @@ x.dates = as.Date(x[,1], format="%d/%m/%Y")
 x.data = apply(x[,-1], MARGIN=2, FUN=function(x){as.numeric(sub("%","", x, fixed=TRUE))/100}) # get rid of percentage signs
 edhec = xts(x.data, order.by=x.dates)
 colnames(edhec)
+
+# calculate a wealth index
+edhec.idx = apply(edhec, MARGIN=2, FUN=function(x){cumprod(1 + na.omit(x))})
+# identify quarters
+edhec.Q.idx=edhec.idx[endpoints(edhec.idx, on="quarters"),]
+# calculate quarterly returns
+edhec.Q.R=ROC(edhec.Q.idx)
+# trim the last data point, if needed
+# dim(edhec.Q.R)
+# edhec.Q.R=edhec.Q.R[-61,]
+# reclass the object
+edhec.Q.R=as.xts(edhec.Q.R)
+# lm requires safe names
+colnames(edhec.Q.R)=make.names(colnames(edhec))
+
