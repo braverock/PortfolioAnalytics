e58f546b6821b856bde3bfb058a5a23e8252ec26
Hezky Varon <hyv@uw.edu>
2012-08-20 18:47:57 +0000

    - added extractStats for ROI
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PortfolioAnalytics@2256 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/extractstats.R
+++ R/extractstats.R
@@ -178,3 +178,24 @@ extractWeights.rebal <- function(RebalResults, ...){
   result = as.xts(result)
   return(result)
 }
+
+
+#' extract some stats from a portfolio list run with DEoptim via
+#' \code{\link{optimize.portfolio}}
+#' 
+#' This function will take everything in the objective_measures slot and \code{unlist} it.  
+#' This may produce a very large number of columns or strange column names.
+#' 
+#' @param object list returned by optimize.portfolio
+#' @param prefix prefix to add to output row names
+#' @param ... any other passthru parameters
+#' @export 
+extractStats.optimize.portfolio.ROI <- function(object, prefix=NULL, ...) {
+  
+  trow<-c(out=object$out, object$weights)
+  result<-trow
+  
+  rnames<-c('out',paste('w',names(object$weights),sep='.'))
+  names(result)<-rnames
+  return(result)
+}
