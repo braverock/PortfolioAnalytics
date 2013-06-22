0ebe5b9ef819819f0d86b82f8dc5cec19031c046
Ross Bennett <rossbennett34@gmail.com>
2013-06-22 00:26:31 +0000

    adding a turnover objective to objectiveFUN.R. Note objectiveFUN.R is a new file for functions used as objectives.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PortfolioAnalytics@2397 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/objectiveFUN.R
@@ -0,0 +1,19 @@
+
+#' Calculates turnover given two vectors of weights.
+#' This is used as an objective function and is called when the user adds an objective of type turnover with \code{\link{add.objective}}
+#' @param weights vector of weights from optimization
+#' @param wts.init vector of initial weights used to calculate turnover from
+#' @author Ross Bennett
+turnover <- function(weights, wts.init=NULL) {
+  # turnover function from https://r-forge.r-project.org/scm/viewvc.php/pkg/PortfolioAnalytics/sandbox/script.workshop2012.R?view=markup&root=returnanalytics
+  
+  # Check that weights and wts.init are the same length
+  if(length(weights) != length(wts.init)) stop("weights and wts.init are not the same length")
+  
+  # If wts.init is not given, then assume a vector of equal weights
+  if(is.null(wts.init)) {
+    N <- length(weights)
+    wts.init <- rep(1/N, N)
+  }
+  return(sum(abs(wts.init-weights))/N)
+}
