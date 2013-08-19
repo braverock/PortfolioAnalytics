f7a454e4612e37db14e77bbc95ad30c207c0dba5
Ross Bennett <rossbennett34@gmail.com>
2013-08-19 05:23:27 +0000

    modifying demo scripts that use the v1 constraints object so that they no longer use functions with a _v1 suffix
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PortfolioAnalytics@2823 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- demo/testing_GenSA.R
+++ demo/testing_GenSA.R
@@ -23,10 +23,10 @@ funds <- names(R)
 mu.port <- mean(colMeans(R))
 
 gen.constr <- constraint(assets = funds, min=-2, max=2, min_sum=0.99, max_sum=1.01, risk_aversion=1)
-gen.constr <- add.objective_v1(constraints=gen.constr, type="return", name="mean", enabled=FALSE, target=mu.port)
-gen.constr <- add.objective_v1(constraints=gen.constr, type="risk", name="var", enabled=FALSE, risk_aversion=10)
-gen.constr <- add.objective_v1(constraints=gen.constr, type="risk", name="CVaR", enabled=FALSE)
-gen.constr <- add.objective_v1(constraints=gen.constr, type="risk", name="sd", enabled=FALSE)
+gen.constr <- add.objective(constraints=gen.constr, type="return", name="mean", enabled=FALSE, target=mu.port)
+gen.constr <- add.objective(constraints=gen.constr, type="risk", name="var", enabled=FALSE, risk_aversion=10)
+gen.constr <- add.objective(constraints=gen.constr, type="risk", name="CVaR", enabled=FALSE)
+gen.constr <- add.objective(constraints=gen.constr, type="risk", name="sd", enabled=FALSE)
 
 
 # =====================
@@ -37,14 +37,14 @@ max.port$max <- rep(0.30,N)
 max.port$objectives[[1]]$enabled <- TRUE
 max.port$objectives[[1]]$target <- NULL
 max.port$objectives[[1]]$multiplier  <- -1
-max.solution <- optimize.portfolio_v1(R, max.port, "GenSA", trace=TRUE)
+max.solution <- optimize.portfolio(R=R, constraints=max.port, optimize_method="GenSA", trace=TRUE)
 
 
 # =====================
 # Mean-variance:  Fully invested, Global Minimum Variance Portfolio
 gmv.port <- gen.constr
 gmv.port$objectives[[4]]$enabled <- TRUE
-gmv.solution <- optimize.portfolio_v1(R, gmv.port, "GenSA", trace=TRUE)
+gmv.solution <- optimize.portfolio(R=R, constraints=gmv.port, optimize_method="GenSA", trace=TRUE)
 
 
 
@@ -56,7 +56,7 @@ cvar.port$min <- rep(0,N)
 cvar.port$max <- rep(1,N)
 cvar.port$objectives[[3]]$enabled <- TRUE
 cvar.port$objectives[[3]]$arguments <- list(p=0.95, clean="boudt")
-cvar.solution <- optimize.portfolio_v1(R, cvar.port, "GenSA")
+cvar.solution <- optimize.portfolio(R=R, constraints=cvar.port, optimize_method="GenSA")
 
 
 
--- demo/testing_pso.R
+++ demo/testing_pso.R
@@ -22,10 +22,10 @@ funds <- names(R)
 mu.port <- mean(colMeans(R))
 
 gen.constr <- constraint(assets = funds, min=-2, max=2, min_sum=0.99, max_sum=1.01, risk_aversion=1)
-gen.constr <- add.objective_v1(constraints=gen.constr, type="return", name="mean", enabled=FALSE, target=mu.port)
-gen.constr <- add.objective_v1(constraints=gen.constr, type="risk", name="var", enabled=FALSE, risk_aversion=10)
-gen.constr <- add.objective_v1(constraints=gen.constr, type="risk", name="CVaR", enabled=FALSE)
-gen.constr <- add.objective_v1(constraints=gen.constr, type="risk", name="sd", enabled=FALSE)
+gen.constr <- add.objective(constraints=gen.constr, type="return", name="mean", enabled=FALSE, target=mu.port)
+gen.constr <- add.objective(constraints=gen.constr, type="risk", name="var", enabled=FALSE, risk_aversion=10)
+gen.constr <- add.objective(constraints=gen.constr, type="risk", name="CVaR", enabled=FALSE)
+gen.constr <- add.objective(constraints=gen.constr, type="risk", name="sd", enabled=FALSE)
 
 
 # =====================
@@ -36,14 +36,14 @@ max.port$max <- rep(0.30,N)
 max.port$objectives[[1]]$enabled <- TRUE
 max.port$objectives[[1]]$target <- NULL
 max.port$objectives[[1]]$multiplier  <- -1
-max.solution <- optimize.portfolio_v1(R, max.port, "pso", trace=TRUE)
+max.solution <- optimize.portfolio(R=R, constraints=max.port, optimize_method="pso", trace=TRUE)
 
 
 # =====================
 # Mean-variance:  Fully invested, Global Minimum Variance Portfolio
 gmv.port <- gen.constr
 gmv.port$objectives[[4]]$enabled <- TRUE
-gmv.solution <- optimize.portfolio_v1(R, gmv.port, "pso", trace=TRUE)
+gmv.solution <- optimize.portfolio(R=R, constraints=gmv.port, optimize_method="pso", trace=TRUE)
 
 
 
@@ -55,7 +55,7 @@ cvar.port$min <- rep(0,N)
 cvar.port$max <- rep(1,N)
 cvar.port$objectives[[3]]$enabled <- TRUE
 cvar.port$objectives[[3]]$arguments <- list(p=0.95, clean="boudt")
-cvar.solution <- optimize.portfolio_v1(R, cvar.port, "pso", trace=TRUE)
+cvar.solution <- optimize.portfolio(R=R, constraints=cvar.port, optimize_method="pso", trace=TRUE)
 
 
 
