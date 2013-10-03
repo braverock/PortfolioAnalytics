0209c748c08e57c4ea7912cbc55a1f19fbe3863c
Peter Carl <pcarl@gsb.uchicago.edu>
2013-10-03 14:19:32 +0000

    - automates file conversion from pdf to png using Imagemagick's convert
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PortfolioAnalytics@3200 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ sandbox/symposium2013/src/pdf2png.sh
@@ -0,0 +1,21 @@
+# #! /bin/bash
+# Automate file conversion in bash
+if [ "$1" == "" ]; then
+  inputdir="../results"
+  destdir="../docs/symposium-slides-2013-figure"
+else
+  inputdir=$1
+  destdir=$2
+fi
+n=0;
+echo " ";
+echo "Converting:"
+echo " ";
+for file in $inputdir/*.pdf
+  do 
+    pngfile=${file%%.pdf}.png
+    echo "$((++n)): $file to $destdir/$pngfile."
+    convert -density 300 $file -quality 100 -sharpen 0x1.0 $destdir/$pngfile 
+    echo " ";
+    echo "$((n)) files were converted from PDF to PNG format."
+done 
