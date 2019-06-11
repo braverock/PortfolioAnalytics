0245d0e70023b41a7ca3d8b7d20b95eff9867af3
Shawn Feng <xf10@illinois.edu>
2019-06-11 15:11:14 -0500

    Update optimize.portfolio.R

--- R/optimize.portfolio.R
+++ R/optimize.portfolio.R
@@ -739,7 +739,7 @@ optimize.portfolio <- optimize.portfolio_v2 <- function(
   weights <- NULL 
   
   # Get the constraints from the portfolio object
-  constraints <- get_constraints(portfolio)
+  constraints <-PortfolioAnalytics:::get_constraints(portfolio)
   
   # set portfolio moments only once
   # For set.portfolio.moments, we are passing the returns,
@@ -759,7 +759,7 @@ optimize.portfolio <- optimize.portfolio_v2 <- function(
   
   # match the args for momentFUN
   .formals <- formals(momentFUN)
-  .formals <- modify.args(formals=.formals, arglist=list(...), dots=TRUE)
+  .formals <- PortfolioAnalytics:::modify.args(formals=.formals, arglist=list(...), dots=TRUE)
   # ** pass ROI=TRUE to set.portfolio.moments so the moments are not calculated
   if(optimize_method %in% c("ROI", "quadprog", "glpk", "symphony", "ipop")){
     obj_names <- unlist(lapply(portfolio$objectives, function(x) x$name))
@@ -767,8 +767,8 @@ optimize.portfolio <- optimize.portfolio_v2 <- function(
       .formals <- modify.args(formals=.formals, arglist=list(ROI=TRUE), dots=TRUE)
     }
   }
-  if("R" %in% names(.formals)) .formals <- modify.args(formals=.formals, arglist=NULL, R=R, dots=FALSE)
-  if("portfolio" %in% names(.formals)) .formals <- modify.args(formals=.formals, arglist=NULL, portfolio=portfolio, dots=FALSE)
+  if("R" %in% names(.formals)) .formals <- PortfolioAnalytics:::modify.args(formals=.formals, arglist=NULL, R=R, dots=FALSE)
+  if("portfolio" %in% names(.formals)) .formals <- PortfolioAnalytics:::modify.args(formals=.formals, arglist=NULL, portfolio=portfolio, dots=FALSE)
   .formals$... <- NULL
   
   # call momentFUN
@@ -1322,6 +1322,30 @@ optimize.portfolio <- optimize.portfolio_v2 <- function(
     }
     
   } ## end case for GenSA
+  
+  ## case if method=Rglpk--- R/GNU Linear Programming Kit Interface
+  if(optimize_method=="Rglpk"){
+    alpha <- list(...)$alpha
+    rmin <- list(...)$rmin
+    mu <- apply(R, 2, mean)
+    Amat <- cbind(rbind(matrix(1, 2, N), mu, as.matrix(R)), rbind(matrix(0, 3, T), diag(1, T)), c(0,0,0,rep(1,T)))
+    objL <- c(rep(0, N), rep(-1/(alpha*T), T), -1)
+    bev <- c(constraints$max_sum, constraints$min_sum, rmin, rep(0,T))
+    
+    dir.vec <- c("<=", ">=", "==", rep(">=", T))
+    
+    bounds <- list(lower = list(ind = 1:N, val = constraints$min),
+                  upper = list(ind = 1:N, val = constraints$max))
+    result <- Rglpk_solve_LP(obj = objL, mat = Amat, dir = dir.vec, rhs = bev, 
+                             types = rep("C",length(objL)), max = TRUE, bound = bounds)
+    w <- as.numeric(result$solution)
+    CVaR <- as.numeric(result$solution[length(objL)])
+    
+    out = list(weights=w[1:N], 
+               CVaR= sum(objL * w),
+               VaR = w[length(w)],
+               call=call)
+  }
   
   # Prepare for final object to return
   end_t <- Sys.time()
