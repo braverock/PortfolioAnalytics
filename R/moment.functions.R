7ae9d1742b193b7cec81443a6d6803db02da4b50
LukeKang98@hotmail.com <56117464+lukekang98@users.noreply.github.com>
2022-07-23 22:49:33 -0700

    fix problem of parameters having same name

--- R/moment.functions.R
+++ R/moment.functions.R
@@ -242,7 +242,7 @@ set.portfolio.moments <- set.portfolio.moments_v2 <- function(R,
              if(hasArg(partial.impute)) partial.impute=match.call(expand.dots=TRUE)$partial.impute else partial.impute=FALSE
              if(hasArg(tol)) tol=match.call(expand.dots=TRUE)$tol else tol=1e-4
              if(hasArg(maxiter)) maxiter=match.call(expand.dots=TRUE)$maxiter else maxiter=150
-             if(hasArg(method)) method=match.call(expand.dots=TRUE)$method else method="bisquare"
+             if(hasArg(method)) loss=match.call(expand.dots=TRUE)$loss else loss="bisquare"
              if(hasArg(init)) init=match.call(expand.dots=TRUE)$init else init="emve_c"
              if(hasArg(mu0)) mu0=match.call(expand.dots=TRUE)$mu0 else mu0=NULL
              if(hasArg(S0)) S0=match.call(expand.dots=TRUE)$S0 else S0=NULL
@@ -250,7 +250,7 @@ set.portfolio.moments <- set.portfolio.moments_v2 <- function(R,
              
              rbTSGS <- tsgs.moments(R=tmpR, filter=filter,
                                     partial.impute=partial.impute, 
-                                    tol=tol, maxiter=maxiter, method=method,
+                                    tol=tol, maxiter=maxiter, method=loss,
                                     init=init, mu0=NULL, S0=NULL)
            }
         
