3138c071d849d5b368d5254445680604e993d10d
LukeKang98@hotmail.com <56117464+lukekang98@users.noreply.github.com>
2022-07-24 15:04:20 -0700

    Update manual page

--- R/robust.moments.R
+++ R/robust.moments.R
@@ -1,15 +1,23 @@
 
-#' Title
 #'
-#' @param R 
-#' @param type 
-#' @param maxit 
-#' @param tol 
 #'
-#' @return
-#' @export
 #'
-#' @examples
+#'
+#' This is a function that makes use of covRob from package RobStatTM to calculate
+#' robust estimators of moments, including mean and variance.
+#'
+#' @param R xts object of asset returns
+#' @param type a string indicating which estimator to compute.
+#'             Valid options are "Rocke" for Rocke’s S-estimator, 
+#'             "MM" for an MM-estimator with a SHR rho function, 
+#'             or "auto" (default) which selects "Rocke" if the 
+#'             number of variables is greater than or equal to 10, 
+#'             and "MM" otherwise.
+#' @param maxit maximum number of iterations
+#' @param tol tolerance for convergence
+#'
+#' @return estimators of moments
+#'
 robust.moments <- function(R, type="auto", maxit=50, tol=1e-4){
   
   robustCov <- RobStatTM::covRob(X=R, type=type, maxit=maxit, tol=tol)
--- R/tsgs.moments.R
+++ R/tsgs.moments.R
@@ -1,16 +1,21 @@
-#' Title
+#' 
+#' 
+#' 
+#' This is a function making use of TSGS function from GSE package to compute
+#' the Two-Step Generalized S-Estimate (2SGS) – a robust estimate of location 
+#' and scatter for data with cell-wise and case-wise contamination.
 #'
-#' @param x 
-#' @param filter 
-#' @param partial.impute 
-#' @param tol 
-#' @param maxiter 
-#' @param method 
-#' @param init 
-#' @param mu0 
-#' @param S0 
+#' @param R xts object of asset returns
+#' @param filter the filter to be used in the first step. Available choices are 
+#'               "UBF-DDC","UBF","DDC","UF". The default one is "UBF-DDC".
+#' @param partial.impute whether partial imputation is used prior to estimation.
+#'                       The default is FALSE.
+#' @param tol tolerance for the convergence criterion. Default is 1e-4.
+#' @param maxiter maximum number of iterations. Default is 150
+#' @param method loss function to use, "bisquare" or "rocke". Default is "bisquare"
+#' @param init type of initial estimator. Options include "emve", "qc", "huber","imputed","emve_c"
 #'
-#' @return
+#' @return estimators of first and second moments
 #' @export
 #'
 #' @examples
@@ -18,9 +23,11 @@ tsgs.moments <- function(R, filter="UBF-DDC",
                          partial.impute=FALSE, tol=1e-4, maxiter=150, 
                          method="bisquare",
                          init="emve_c"){
+  
   tsgsRob <- GSE::TSGS(x=R, filter=filter,
                        partial.impute=partial.impute, tol=tol, maxiter=maxiter, method=method,
                        init=init)
+  
   return(list(mu=tsgsRob@mu, sig=tsgsRob@S))
   
 }
