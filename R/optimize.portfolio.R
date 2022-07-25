51a4cb4804075a42b0938f1ce080c6adb3d05749
Xinran Zhao <zhaoxinran1228@gmail.com>
2022-07-25 12:28:06 -0400

    test return_target constraint

--- R/optimize.portfolio.R
+++ R/optimize.portfolio.R
@@ -2849,8 +2849,8 @@ optimize.portfolio <- optimize.portfolio_v2 <- function(
     
     ## turnover constraint ROI cannot
     
-    ## target return constraint
-    # constraints_cvxr = append(constraints_cvxr, t(mout$mu) %*% wts >= constraints$return_target)
+    ## target return constraint add mout$mu xinran
+    constraints_cvxr = append(constraints_cvxr, t(mout$mu) %*% wts >= constraints$return_target)
     
     ## factor exposure constraint
     # constraints_cvxr = append(constraints_cvxr, wts >= constraints$B - constraints$lower)
