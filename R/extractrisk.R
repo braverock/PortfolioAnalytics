7f0f6606bd456f423a543611af43a517229178c9
xinran <xzhao6@uw.edu>
2023-02-15 11:08:36 -0800

    fix alpha in extract_risk

--- R/extractrisk.R
+++ R/extractrisk.R
@@ -3,18 +3,13 @@
 #' @param portfolio an object of type "portfolio" specifying the constraints and objectives for the optimization
 #' @param w the weight of the portfolio
 #' @export extract_risk
-extract_risk <- function(R, w, portfolio=NULL){
+extract_risk <- function(R, w, ES_alpha = 0.05, EQS_alpha = 0.05){
   res = list()
   res$mean = mean(R %*% w)
   res$StdDev = sqrt(t(w) %*% cov(R) %*% w)
   
-  alpha = 0.05
-  for (objective in portfolio$objectives){
-    if (objective$enabled){
-      alpha <- ifelse(!is.null(objective$arguments[["p"]]), objective$arguments[["p"]], alpha)
-    }
-  }
-  if(alpha > 0.5) alpha <- (1 - alpha)
+  if(ES_alpha > 0.5) ES_alpha <- (1 - ES_alpha)
+  if(EQS_alpha > 0.5) EQS_alpha <- (1 - EQS_alpha)
   
   # ES/EQS by CVXR
   T <- dim(R)[1]
@@ -23,14 +18,14 @@ extract_risk <- function(R, w, portfolio=NULL){
   z <- CVXR::Variable(T)
   
   ## ES
-  obj_es <- zeta + (1/(T*alpha)) * sum(z)
+  obj_es <- zeta + (1/(T*ES_alpha)) * sum(z)
   con_es <- list(z >= 0, z >= -X %*% w - zeta)
   p_es <- CVXR::Problem(CVXR::Minimize(obj_es), constraints = con_es)
   res_es = CVXR::solve(p_es)
   res$ES = res_es$value
   
   ## EQS
-  obj_eqs <- zeta + (1/alpha) * CVXR::p_norm(z, p=2)
+  obj_eqs <- zeta + (1/EQS_alpha) * CVXR::p_norm(z, p=2)
   con_eqs = list(z >= 0, z >= -X %*% w - zeta)
   p_eqs <- CVXR::Problem(CVXR::Minimize(obj_eqs), constraints = con_eqs)
   res_eqs = CVXR::solve(p_eqs)
