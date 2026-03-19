68e7431a05f3e6e7f107e9d4104826421d5cdd3d
Brian G. Peterson <brian@braverock.com>
2026-03-18 19:02:42 -0500

    update to use new psolve in CVXR since solve is deprecated

--- R/extractrisk.R
+++ R/extractrisk.R
@@ -19,42 +19,41 @@
 #' @param EQS_alpha the default value is 0.05, but could be specified as any value between 0 and 1
 #' @param moment_setting the default is NULL, should provide moment_setting=list(mu=, sigma=) if customize momentFUN
 #' @export extract_risk
-extract_risk <- function(R, w, ES_alpha = 0.05, CSM_alpha = 0.05, EQS_alpha = 0.05, moment_setting = NULL){
-  res = list()
-  if(is.null(moment_setting$mu)) res$mean = mean(R %*% w) else res$mean = moment_setting$mu %*% w
-  if(is.null(moment_setting$sigma)) res$StdDev = sqrt(t(w) %*% cov(R) %*% w) else res$StdDev = sqrt(t(w) %*% moment_setting$sigma %*% w)
-  
-  if(ES_alpha > 0.5) ES_alpha <- (1 - ES_alpha)
-  if(CSM_alpha > 0.5) CSM_alpha <- (1 - CSM_alpha)
-  if(EQS_alpha > 0.5) EQS_alpha <- (1 - EQS_alpha)
-  
+extract_risk <- function(R, w, ES_alpha = 0.05, CSM_alpha = 0.05, EQS_alpha = 0.05, moment_setting = NULL) {
+  res <- list()
+  if (is.null(moment_setting$mu)) res$mean <- mean(R %*% w) else res$mean <- moment_setting$mu %*% w
+  if (is.null(moment_setting$sigma)) res$StdDev <- sqrt(t(w) %*% cov(R) %*% w) else res$StdDev <- sqrt(t(w) %*% moment_setting$sigma %*% w)
+
+  if (ES_alpha > 0.5) ES_alpha <- (1 - ES_alpha)
+  if (CSM_alpha > 0.5) CSM_alpha <- (1 - CSM_alpha)
+  if (EQS_alpha > 0.5) EQS_alpha <- (1 - EQS_alpha)
+
   # ES/CSM by CVXR
   T <- dim(R)[1]
   X <- as.matrix(R)
   zeta <- CVXR::Variable(1)
   z <- CVXR::Variable(T)
-  
+
   ## ES
-  obj_es <- zeta + (1/(T*ES_alpha)) * sum(z)
+  obj_es <- zeta + (1 / (T * ES_alpha)) * sum(z)
   con_es <- list(z >= 0, z >= -X %*% w - zeta)
   p_es <- CVXR::Problem(CVXR::Minimize(obj_es), constraints = con_es)
-  res_es = CVXR::solve(p_es, solver = "ECOS")
-  res$ES = res_es$value
-  
+  res_es <- CVXR::psolve(p_es, solver = "ECOS")
+  res$ES <- res_es # $value update for psolve
+
   ## CSM
-  obj_CSM <- zeta + (1/(sqrt(T)*CSM_alpha)) * CVXR::p_norm(z, p=2)
-  con_CSM = list(z >= 0, z >= -X %*% w - zeta)
+  obj_CSM <- zeta + (1 / CSM_alpha) * CVXR::p_norm(z, p = 2)
+  con_CSM <- list(z >= 0, z >= -X %*% w - zeta)
   p_CSM <- CVXR::Problem(CVXR::Minimize(obj_CSM), constraints = con_CSM)
-  res_CSM = CVXR::solve(p_CSM, solver = "ECOS")
-  res$CSM = res_CSM$value
-  
+  res_CSM <- CVXR::psolve(p_CSM, solver = "ECOS")
+  res$CSM <- res_CSM # $value
+
   ## EQS
-  obj_EQS <- zeta + (1/(T*EQS_alpha)) * sum(CVXR::pos(square(CVXR::pos(X %*% w)) - zeta))
-  con_EQS = list()
+  obj_EQS <- zeta + (1 / (T * EQS_alpha)) * sum(CVXR::pos(square(CVXR::pos(X %*% w)) - zeta))
+  con_EQS <- list()
   p_EQS <- CVXR::Problem(CVXR::Minimize(obj_EQS), constraints = con_EQS)
-  res_EQS = CVXR::solve(p_EQS, solver = "ECOS")
-  res$EQS = res_EQS$value
-  
+  res_EQS <- CVXR::psolve(p_EQS, solver = "ECOS")
+  res$EQS <- res_EQS # $value
+
   res
 }
-
